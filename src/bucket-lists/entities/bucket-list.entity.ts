4e2a1e05d40cebf319f34d894b2ff3b3cced5d95
bjl0615 <bjl0615@gmail.com>
2024-12-30 18:39:25 +0900

    DB Modelling & Modul Create

---
+++ src/bucket-lists/entities/bucket-list.entity.ts
@@ -0,0 +1,25 @@
+import { BucketListItem } from "./../../bucket-list-items/entities/bucket-list-item.entity";
+import { User } from "src/users/entities/user.entity";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class BucketList {
+  @PrimaryGeneratedColumn()
+  id : number;
+
+  @Column({
+    type : 'varchar',
+    length : 255,
+    nullable : false
+  })
+  name : string;
+
+  @ManyToOne(() => User, (user) => user.bucketLists)
+  user: User;
+
+  @OneToMany(
+    () => BucketListItem,
+    (bucketListItem) => bucketListItem.bucketList,
+  )
+  items: BucketListItem[];
+}
